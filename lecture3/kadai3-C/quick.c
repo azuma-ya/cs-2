b00356ffc0c8ddfb875aed1d5aa1d3336a45936e
azuma-ya <kazu.bluemountain@gmail.com>
2023-12-20 14:14:49 +0900

    クイックソートをリファクトする

--- lecture3/kadai3-C/quick.c
+++ lecture3/kadai3-C/quick.c
@@ -21,29 +21,25 @@ void swap(int *a, int *b)
 
 int partition(int arr[], int low, int high)
 {
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++)
+    for (int i = low; i < high; i++)
     {
-        if (arr[j] <= pivot)
+        if (arr[i] <= arr[high])
         {
-            i++;
-            swap(&arr[i], &arr[j]);
+            swap(&arr[low], &arr[i]);
+            low++;
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+    swap(&arr[low], &arr[high]);
+    return low;
 }
 
 void quick_sort(int arr[], int low, int high)
 {
     if (low < high)
     {
-        int pi = partition(arr, low, high);
-
-        quick_sort(arr, low, pi - 1);
-        quick_sort(arr, pi + 1, high);
+        int pivot = partition(arr, low, high);
+        quick_sort(arr, low, pivot - 1);
+        quick_sort(arr, pivot + 1, high);
     }
 }
 
